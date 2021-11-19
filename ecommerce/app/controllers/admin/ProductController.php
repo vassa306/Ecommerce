c9e1daac4c5b187f7e020ee6566e646ca20a57f1
vj <vassa306@gmail.com>
2021-11-19 21:07:56 +0100

    create ProductController

---
+++ ecommerce/app/controllers/admin/ProductController.php
@@ -0,0 +1,166 @@
+<?php
+namespace app\controllers\admin;
+
+use app\classes\CSRFToken;
+use app\classes\Request;
+use app\classes\Session;
+use app\models\Category;
+use app\models\SubCategory;
+use app\classes\ValidateRequest;
+use app\classes\Redirect;
+
+class ProductController
+{
+
+    public $table_name = 'categories';
+
+    public $categories;
+
+    public $links;
+
+    public $subcategories;
+    
+    public $subcategories_links;
+    
+    public function __construct()
+    {
+        $total = Category::all()->count();
+        $subtotal = SubCategory::all()->count();
+        $object = new Category();
+        list ($this->categories, $this->links) = paginate(5, $total, $this->table_name, $object);
+        list ($this->subcategories, $this->subcategories_links) = paginate(5, $subtotal, 'sub_categories', new SubCategory);
+    }
+
+    public function showCreateProductForm()
+    {
+        return view('admin/products/create');
+    }
+
+    public function store()
+    {
+        if (Request::has('post')) {
+            $request = Request::get('post');
+
+            if (CSRFToken::verifyCSRFToken($request->token)) {
+                $rules = [
+                    'name' => [
+                        'required' => true,
+                        'minLength' => 3,
+                        'string' => true,
+                        'unique' => 'categories'
+                    ]
+                ];
+                $validate = new ValidateRequest();
+                $validate->abide($_POST, $rules);
+                if ($validate->hasError()) {
+                    $errors = $validate->getErrorMsgs();
+                    return view('admin/products/categories', [
+                        'categories' => $this->categories,
+                        'links' => $this->links,
+                        'errors' => $errors,
+                        'subcategories' => $this->subcategories,
+                        'subcategories_links' => $this->subcategories_links
+                        
+                    ]);
+                }
+
+                // process form data
+                Category::Create([
+                    'name' => $request->name,
+                    'slug' => slug($request->name)
+                ]);
+                $total = Category::all()->count();
+                $subtotal = SubCategory::all()->count();
+                list ($this->categories, $this->links) = paginate(5, $total, $this->table_name, new Category());
+                list ($this->subcategories, $this->subcategories_links) = paginate(5, $subtotal, 'sub_categories', new SubCategory);
+
+                return view('admin/products/categories', [
+                    'categories' => $this->categories,
+                    'links' => $this->links,
+                    'success' => 'Category Created',
+                    'subcategories' => $this->subcategories,
+                    'subcategories_links' => $this->subcategories_links
+                ]);
+            }
+
+            throw new \Exception('Token Mismatch');
+        }
+        return null;
+    }
+
+    public function edit($id)
+    {//var_dump($id);
+        if (Request::has('post')) {
+            $request = Request::get('post');
+            $extra_errors = [];
+//             echo json_encode([
+//                 'success' => 'OK '.__LINE__." category: ".$id//print_r([$validate->hasError() , $duplicate_subcategory , $category],true)
+//             ]);exit;
+            
+            if (CSRFToken::verifyCSRFToken($request->token, false)) {
+                $rules = [
+                    'name' => [
+                        'required' => true,
+                        'minLength' => 3,
+                        'string' => true,
+                        'unique' => 'categories'
+                    ]
+                ];
+                $validate = new ValidateRequest();
+                $validate->abide($_POST, $rules);
+                /*get id from array*/
+                
+                $category = Category::where('id', $id)->exists();
+                if (! $category) {
+                    $extra_errors['name'] = array(
+                        'Invalid Product Category'
+                    );
+                }
+                
+                
+                if ($validate->hasError() || !$category) {
+                    $errors = $validate->getErrorMsgs();
+                    count($extra_errors) ? $response = array_merge($errors, $extra_errors) : $response = $errors;
+                    header('HTTP/1.1 422 Unprocessable Entity', true, 422);
+                    echo json_encode($response);
+                    exit();
+                }
+                
+                Category::where('id', $id)->update([
+                    'name' => $request->name
+                ]);
+                echo json_encode([
+                    'success' => 'Record Updated Sucessfully'
+                ]);
+                exit();
+
+                // process form data
+            }
+
+            throw new \Exception('Token Mismatch');
+        }
+        return null;
+    }
+
+    public function delete($id)
+    {
+        if (Request::has('post')) {
+            $request = Request::get('post');
+            if (CSRFToken::verifyCSRFToken($request->token)) {
+                Category::destroy($id);
+                $subcategories = SubCategory::where('category_id',$id)->get();            
+                if (count($subcategories)) {
+                    foreach ($subcategories as $subcategory){
+                        $subcategory->delete();
+                    }
+                    
+                }
+                Session::add('success', 'Category has been deleted');
+                Redirect::to('/admin/products/categories');
+            }
+            throw new \Exception('Token Mismatch');
+        }
+        return null;
+    }
+}
+
---
+++ ecommerce/resources/views/admin/products/create.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layout.base') 
+@section('title','Create Product')
+@section('data-page-id','adminProducts') 
+
+<!-- at first use isset - test if variable exists in page -->
+@section('content')
+	<div class="add-product">
+		<div class="row expanded">
+		   <div class="column medium-11">
+				<h2>Add Inventory Item</h2>
+			</div>
+		</div>
+
+		@include('includes.message')
+		<form method="post" action="/admin/products/create">
+			<div class ="small-12 medium-11">
+				<div class="row expanded">
+					<div class="small-12 medium-6 column">
+					  	<label>Product Name:
+					  		<input type="text" name="name" placeholder="Product Name" 
+					  		value="{{ \app\classes\Request::old('post','name') }}">
+					  	</label>
+					</div>
+			
+									
+			
+					<div class="small-12 medium-6 column">
+					  	<label>Product Price:
+					  		<input type="text" name="price" placeholder="Product Price" 
+					  		value="{{ \app\classes\Request::old('post','price') }}">
+					  	</label>
+					</div>
+				</div>
+			</div>		
+			
+		</form>
+
+	@include('includes.delete-modal') 
+@endsection
