c354811fbd97d8fffd96a2ba18525c613e31ed8e
vj <vassa306@gmail.com>
2022-02-17 01:51:58 +0100

    test commit last

--- ecommerce/app/classes/CSRFToken.php
+++ ecommerce/app/classes/CSRFToken.php
@@ -1,6 +1,6 @@
 <?php
 namespace app\classes;
-
+//test
 class CSRFToken
 {
 
--- ecommerce/app/config/_env.php
+++ ecommerce/app/config/_env.php
@@ -3,4 +3,3 @@ define('BASE_PATH', realpath(__DIR__ . '/../../'));
 require_once __DIR__ . '/../../vendor/autoload.php';
 $dotEnv = $dotEnv = \Dotenv\Dotenv::create(BASE_PATH);
 $dotEnv->load();
-//test test
