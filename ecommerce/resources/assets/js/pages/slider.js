fa569d4f6fced96c628a4541de9f6606d52d90a4
vj <vassa306@gmail.com>
2022-04-01 10:31:29 +0200

    add slider into Frontend

---
+++ ecommerce/resources/assets/js/pages/slider.js
@@ -0,0 +1,15 @@
+//create a function for slider
+(function(){
+	'use strict';
+	ACMESTORE.homeslider.initCarousel = function(){
+		$('.hero-slider').slick({
+			slidesToShow:1,
+			autoplay: true,
+			arrows: false,
+			dots: false,
+			fade: true,
+			autoplayHoverPause: true,
+			slideToScroll: 1,
+		});
+	};
+})();
